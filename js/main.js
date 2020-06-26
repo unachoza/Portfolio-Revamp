1fc6bf8e9c7339b82880d96af93098ede8dd1425
arianna choza <unachoza@gmail.com>
2020-06-26 00:44:33 -0400

    starter files

---
+++ js/main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($){
+	//open navigation clicking the menu icon
+	$('.cd-nav-trigger').on('click', function(event){
+		event.preventDefault();
+		toggleNav(true);
+	});
+	//close the navigation
+	$('.cd-close-nav, .cd-overlay').on('click', function(event){
+		event.preventDefault();
+		toggleNav(false);
+	});
+
+	function toggleNav(bool) {
+		$('.cd-nav-container, .cd-overlay').toggleClass('is-visible', bool);
+		$('main').toggleClass('scale-down', bool);
+	}
+	$('.cd-nav li').on('click', function(event){
+		//close navigation
+		toggleNav(false);
+	});
+});
