204705fe48609c1441d99e53ad39ae935427f3cf
arianna choza <unachoza@gmail.com>
2020-12-21 21:45:46 -0800

    blog post interview

---
+++ blog/node.js
@@ -0,0 +1,34 @@
+const solution = (messages) => {
+  class Emitter {
+    constructor(messages = []) {
+      this.messages = messages;
+      this.event = () => {};
+    }
+
+    setEvent(fn) {
+      this.event = fn;
+    }
+
+    trigger() {
+      this.messages.forEach((message) => this.event(message));
+    }
+  }
+
+  class Receiver {
+    constructor() {
+      this.messages = [];
+    }
+
+    ping(message) {
+      this.messages.push(message);
+    }
+  }
+
+  const myEmitter = new Emitter(messages);
+  const myReceiver = new Receiver();
+
+  myEmitter.setEvent(myReceiver.ping);
+  myEmitter.trigger();
+
+  return myReceiver.messages;
+};
